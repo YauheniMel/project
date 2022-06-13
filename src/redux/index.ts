cd6b92bc367ad2eb5773f79853f8b6846991ad9a
YauheniMel <207melnik@gmail.com>
2022-06-13 20:55:49 +0300

    refactor: Change global state

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,15 +1,16 @@
 import { combineReducers, legacy_createStore as createStore } from 'redux';
 import authReducer from './reducers/auth-reducer';
-import collectionsReducer from './reducers/collections-reducer';
 import userReducer from './reducers/user-reducer';
 
 const reducer = combineReducers({
   auth: authReducer,
   user: userReducer,
-  collections: collectionsReducer,
 });
 
 const store = createStore(reducer);
+
+type RootReducerType = typeof reducer;
+export type AppStateType = ReturnType<RootReducerType>;
 
 declare global {
   interface Window {
